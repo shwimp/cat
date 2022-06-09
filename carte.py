795bb1253cdb34e88bb9da6e4eda852c7beb03f9
shwimp_10 <rc38sw@orange.fr>
2022-06-09 22:49:11 +0200

    correction check_treasure + add turn_right & turn_left

--- carte.py
+++ carte.py
@@ -4,33 +4,36 @@ from unicodedata import name
 
 class Carte(object):
 
-    def __init__(self, map) -> None:
+    def __init__(self, map):
         self.compass = ['N', 'E', 'S', 'O']
         self.map = map
         self.x_size = 0
         self.y_size = 0
-        self.adventurers = {}
-        self.get_adv_info()
         self.map_array = []
         self.draw_map()
+        self.adventurers = {}
+        self.get_adv_info()
 
-    def get_adv_info(self) -> dict:
+    def get_adv_info(self):
         for elem in self.map:
             if elem[0] == "A":
                 name = elem[1]
                 x = int(elem[2])
                 y = int(elem[3])
+                self.check_position()
                 direction = elem[4]
                 move = elem[5]
                 self.adventurers[name] = {"x": x, "y": y, "direction": direction, "move": move, "treasures": 0}
 
+    def check_position(self):
+        pass
 
-    def draw_map(self) -> None:
+    def draw_map(self):
         self.create_map()
         self.add_mountain()
         self.add_treasure()
     
-    def create_map(self) -> None:
+    def create_map(self):
         for elem in self.map:
             if elem[0] == "C":
                 self.x_size = int(elem[1])
@@ -39,7 +42,7 @@ class Carte(object):
                     self.map_array.append(["."] * self.x_size)
 
     
-    def add_mountain(self) -> None:
+    def add_mountain(self):
         for elem in self.map:
             if elem[0] == "M":
                 x = int(elem[1])
@@ -47,7 +50,7 @@ class Carte(object):
                 self.map_array[y][x] = "M"
 
     
-    def add_treasure(self) -> None:
+    def add_treasure(self):
         for elem in self.map:
             if elem[0] == "T":
                 x = int(elem[1])
@@ -73,9 +76,9 @@ class Carte(object):
 
     def check_treasure(self, perso):
         place = self.map_array[perso["y"]][perso["x"]]
-        if place is int and place != 0:
-            place =-1
-            perso["treasures"] =+ 1
+        if type(place) is int and place != 0:
+            self.map_array[perso["y"]][perso["x"]] -= 1
+            perso["treasures"] += 1
 
     def move_on(self, perso):
         direction = perso["direction"]
@@ -103,6 +106,9 @@ class Carte(object):
                     self.check_treasure(perso)
 
     def turn_right(self, perso):
-        pass
+        index = self.compass.index(perso["direction"])
+        perso["direction"] = self.compass[index + 1] if index < len(self.compass) - 1 else self.compass[0]
+
     def turn_left(self, perso):
-        pass
+        index = self.compass.index(perso["direction"])
+        perso["direction"] = self.compass[index - 1]
